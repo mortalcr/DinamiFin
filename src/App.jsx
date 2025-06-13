247fe04c2677edaa0a2ed550f50f9844f7261b29
leiner117 <leineralvarador117@gmail.com>
2025-06-13 17:58:07 -0600

    Remove unused import for MesActual component from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./components/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
-import MesActual from "./components/mesActual";
 import DashboardHistorico from "./pages/DashboardHistorico";
 import Navbar from "./components/Navbar";
 import ImportarDatos from "./components/ImportarDatos";
@@ -18,7 +17,6 @@ function App() {
         <Route path="/" element={token ? <Navigate to="/dashboard" replace /> : <Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/mesActual" element={<MesActual />} />
         <Route path="/dashboardHistorico" element={<DashboardHistorico />} />
         <Route
           path="/importar"
