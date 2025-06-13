d2ff28e154817217758f35f59816ff2b3a653d82
leiner117 <leineralvarador117@gmail.com>
2025-06-12 21:03:40 -0600

    fix: Remove unused imports from App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./components/Dashboard";
 import MesActual from "./components/mesActual";
 
 function App() {
